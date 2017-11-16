01af7b2f48c3af23ef26057dffd7bb3a9c1e0db4
Kamil Tustanowski <kamil.tustanowski@gmail.com>
2017-11-16 20:37:54 +0100

    Initial Commit

---
+++ UnitTestStories/UnitTestStories.h
@@ -0,0 +1,19 @@
+//
+//  UnitTestStories.h
+//  UnitTestStories
+//
+//  Created by Kamil Tustanowski on 16.11.2017.
+//  Copyright © 2017 Kamil Tustanowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UnitTestStories.
+FOUNDATION_EXPORT double UnitTestStoriesVersionNumber;
+
+//! Project version string for UnitTestStories.
+FOUNDATION_EXPORT const unsigned char UnitTestStoriesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UnitTestStories/PublicHeader.h>
+
+
